9e202e801114eb8d8cc9065003927ee9d160c2d0
attaulhaq <attaulhaq150@gmail.com>
2026-05-05 17:44:54 +0500

    fix: Show full localhost URL in server console output

--- backend/server.js
+++ backend/server.js
@@ -43,4 +43,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-app.listen(PORT, console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
